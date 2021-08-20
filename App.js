c954fda9223a2cd113f536e199bf69dd95cb0819
Arjun Doel <arjundoel@gmail.com>
2021-08-20 12:41:09 +0100

    mini project start

--- App.js
+++ App.js
@@ -1,31 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TouchableHighlight, Image, Button, SafeAreaView } from 'react-native';
-import iconImage from './assets/icon.png'
+import { StyleSheet, SafeAreaView, ImageBackground } from 'react-native';
 
-export default function App() {
-  
+export default function App() {  
 
-  const test = {
-    title: 'this is a title'
-  }
-
-  const handlePress = () => console.log('text pressed');
+  const backgroundImage = 'https://res.cloudinary.com/dlj1sbbtb/image/upload/v1629459599/background_hjbriw.jpg';
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text onPress={handlePress}>{test.title}</Text>
-      <Image source={iconImage} style={styles.img_icon}/>
-      <TouchableHighlight onPress={handlePress}>
-        <Image source={{ uri: 'https://res.cloudinary.com/dlj1sbbtb/image/upload/v1625580751/puppy-4484296_1920_tj3rqw.jpg'}} style={styles.img_icon}/>
-      </TouchableHighlight>
-      <Button 
-        title="Hi" 
-        color="red"
-        onPress={() => alert('sup')}
-        
-        />
-      <StatusBar style="auto" />
+      <ImageBackground source={backgroundImage}>
+
+      </ImageBackground>
     </SafeAreaView>
   );
 }
@@ -33,15 +17,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundImage: '',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
     // fontSize: 100,
   },
-  img_icon: {
-    width: 200,
-    height: 200,
-    margin: 10,
-    borderRadius: 20,
-  }
 });
