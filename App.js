042d198ee9509650b84c982a1181a9a71dd4e34f
Arjun Doel <arjundoel@gmail.com>
2021-08-20 12:22:36 +0100

    tutorial day two

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, Image, Button, SafeAreaView } from 'react-native';
+import iconImage from './assets/icon.png'
 
 export default function App() {
+  
 
-  console.log('hello');
+  const test = {
+    title: 'this is a title'
+  }
+
+  const handlePress = () => console.log('text pressed');
 
   return (
-    <View style={styles.container}>
-      <Text>Yo!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePress}>{test.title}</Text>
+      <Image source={iconImage} style={styles.img_icon}/>
+      <TouchableHighlight onPress={handlePress}>
+        <Image source={{ uri: 'https://res.cloudinary.com/dlj1sbbtb/image/upload/v1625580751/puppy-4484296_1920_tj3rqw.jpg'}} style={styles.img_icon}/>
+      </TouchableHighlight>
+      <Button 
+        title="Hi" 
+        color="red"
+        onPress={() => alert('sup')}
+        
+        />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -20,5 +36,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    // fontSize: 100,
   },
+  img_icon: {
+    width: 200,
+    height: 200,
+    margin: 10,
+    borderRadius: 20,
+  }
 });
